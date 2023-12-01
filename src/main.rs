f71e24d3683617651e2dca36597c405cae34a65e
Mark S <the@wondersmith.dev>
2023-12-01 17:55:59 -0500

    release: complete challenge -1

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,27 @@
 //!
 
 // Third-Party Imports
-use axum::{routing::get, Router};
-
-/// TODO(the-wondersmith): DOCUMENTATION
-#[tracing::instrument]
-async fn hello_world() -> &'static str {
-    "Hello, world!"
-}
+use axum::{http::StatusCode, response::IntoResponse, routing, Router};
 
 /// TODO(the-wondersmith): DOCUMENTATION
 #[tracing::instrument]
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", routing::get(hello_world))
+        .route("/-1/error", routing::get(throw_error));
 
     Ok(router.into())
+}
+
+/// Complete [Challenge -1: Task 1](https://console.shuttle.rs/cch/challenge/-1#:~:text=one%20that%20counts.-,%E2%AD%90,-Task%201%3A%20Everything)
+#[tracing::instrument]
+async fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+/// Complete [Challenge -1: Task 2](https://console.shuttle.rs/cch/challenge/-1#:~:text=the%20bonus%20task.-,%F0%9F%8E%81,-Task%202%3A%20Fake)
+#[tracing::instrument]
+async fn throw_error() -> impl IntoResponse {
+    (StatusCode::INTERNAL_SERVER_ERROR, "Gimme them bonus points")
 }
